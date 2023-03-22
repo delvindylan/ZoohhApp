5e057882d81f2000c8247b61a538fc464da98f44
Delvin <delvin@ngauv.ch>
2023-03-22 13:38:10 +0100

    Homepage + Impressum

--- zooapp-react/src/App.js
+++ zooapp-react/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Impressum from "./components/Impressum";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>ZooApp von Justin Calle, Daniel Bischof, Delvin Ngauv</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>Das wird unsere Zoo App</h2>
+
+          <h3>in developement</h3>
+
+          <Impressum></Impressum>
+
       </header>
     </div>
   );
---
+++ zooapp-react/src/components/Impressum.js
@@ -0,0 +1,15 @@
+export default function Impressum () {
+
+    return (
+        <div>
+
+            <h3>Impressum</h3>
+            <p>Verantwortlich für den Inhalt dieser Website im Rahmen des Schulprojekts sind Delvin Ngauv, Justin Calle und Daniel Bischof. Sie erreichen uns unter der E-Mail-Adresse zoohh.bbw@outlook.com.
+                Diese Website wurde im ist ein Schulprojekt und dient ausschließlich Bildungszwecken. Wir übernehmen keine Haftung für die Richtigkeit und Vollständigkeit der hier bereitgestellten Informationen und behalten uns das Recht vor, jederzeit Änderungen an der Website vorzunehmen.
+
+                Delvin Ngauv, Justin Calle und Daniel Bischof</p>
+
+        </div>
+    )
+
+}
