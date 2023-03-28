71e9916ffd5138e169d41d81c4ec794bb8319060
justindavidcalle <justincalle@outlook.de>
2023-03-28 22:33:34 +0200

    Komponente hinzugefügt

---
+++ zooapp-react/src/pages/Ticket.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+function Ticket(){
+
+    return(
+        <div className="Ticket">
+            <Navbar/>
+            <div className="Ticketliste">
+                
+            </div>
+        </div>
+    )
+}
