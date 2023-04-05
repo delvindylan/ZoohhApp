e2517d588f5d7ab9c353bf5b2704aa6b4fe3a645
David21092 <bischof.david.db@gmail.com>
2023-04-05 10:31:39 +0200

    Delvin

--- zooapp-react/src/App.jsx
+++ zooapp-react/src/App.jsx
@@ -5,21 +5,23 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.js';
 import Impressum from './pages/Impressum'
 import Ticket from './pages/Ticket'
-
+import Kontakt from './pages/Kontakt'
 // Components
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+              <Navbar/>
             <header className="App-header">
-        <Navbar/>
-        
+          
 <div>
         <Routes>
         <Route path='/' element={ <Home/> }/>
           <Route path='/Impressum' element={ <Impressum/> }/>
           <Route path='/Tickets' element={ <Ticket/> }/>
+          <Route path='/Kontakt' element={ <Kontakt/> }/>
+
         </Routes>
     </div>
     </header>
